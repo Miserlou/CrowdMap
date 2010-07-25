356df5d979a2ca2cdc608d706e308437ac63e001
Rich Jones <rich@anomos.info>
2010-07-25 16:19:36 -0400

    alfjkasdf

--- Android/src/org/ale/crowdmap/CMConfigActivity.java
+++ Android/src/org/ale/crowdmap/CMConfigActivity.java
@@ -1,7 +1,13 @@
 package org.ale.crowdmap;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.Window;
 import android.view.View.OnClickListener;
@@ -14,6 +20,8 @@ import android.widget.Spinner;
 import android.widget.AdapterView.OnItemSelectedListener;
 
 public class CMConfigActivity extends Activity {
+	
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,7 +33,7 @@ public class CMConfigActivity extends Activity {
         et.setEnabled(false);
         et.setVisibility(4);
         
-        Spinner s = (Spinner) findViewById(R.id.spinner);
+        final Spinner s = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter adapter = ArrayAdapter.createFromResource(
                 this, R.array.carriers, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -60,8 +68,30 @@ public class CMConfigActivity extends Activity {
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
 				//Do the stuff
+				SharedPreferences prefs;
+				final SharedPreferences.Editor editor;
+				prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+				editor = prefs.edit();
 				
+				String unique = prefs.getString("unique", "");
+				String carrier = prefs.getString("carrier", "");
 				
+				if(unique.equals("")){
+					unique = new BigInteger(130, new SecureRandom()).toString(20);
+					editor.putString("unique", unique);
+				}
+			
+				int pos = s.getSelectedItemPosition();
+				if(pos == 17){
+					carrier = et.getText().toString();
+				}
+				else{
+					carrier = s.getSelectedItem().toString();
+				}
+				editor.putString("carrier", carrier);
+				
+				Intent i = new Intent(null, ReporterService.class);
+				startService(i);
 			}
         	
         });
